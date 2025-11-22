27a8f870ef1706662cfc87d170273c69cbece7c1
nicolelacorte <lacortenicole40@gmail.com>
2025-11-22 14:02:21 +0000

    Update message

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.write("Nicole J. Lacorte\n");
   res.write("BSIT - BA 4102 \n");
-  res.write("The harder you work for something, the greater you’ll feel when you achieve it.\n");
+  res.write("The harder you work for something, the greater you will feel when you achieve it.\n");
   res.end();
 });
 
